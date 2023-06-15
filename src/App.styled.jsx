13fae7c2272d95b8ac644e7f2efaaa36f44b10f3
Oksana Komarnytska <okazimiriv@gmail.com>
2023-06-16 00:34:50 +0200

    2

--- src/App.styled.jsx
+++ src/App.styled.jsx
@@ -1,9 +1,11 @@
 import styled from '@emotion/styled';
 
 export const Container = styled.div`
-  margin: 0 auto;
-  padding: 0px 20px;
-  width: 100%;
+ margin: 100px auto;
+  padding: 30px;
+  width: 480px;  
+  border-radius: 10px;
+  box-shadow: rgba(100, 100, 111, 0.2) 0px 7px 29px 0px;
 `;
 
 // @media screen and (min-width: 480px) {
