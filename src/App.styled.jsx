cbcc2dcd41ad01256ff1838223251ed0872bc84b
Oksana Komarnytska <okazimiriv@gmail.com>
2023-06-15 23:39:46 +0200

    shadow

--- src/App.styled.jsx
+++ src/App.styled.jsx
@@ -1,25 +1,11 @@
 import styled from '@emotion/styled';
 
 export const Container = styled.div`
-  margin: 0 auto;
-  padding: 0px 20px;
-  width: 100%;
+ margin: 100px auto;
+  padding: 30px;
+  width: 480px;  
+  border-radius: 10px;
+  box-shadow: rgba(100, 100, 111, 0.2) 0px 7px 29px 0px;
 `;
 
-// @media screen and (min-width: 480px) {
-//   .Container {
-//     width: 480px;
-//   }
-// }
 
-// @media screen and (min-width: 768px) {
-//   .Container {
-//     width: 768px;
-//   }
-// }
-
-// @media screen and (min-width: 1200px) {
-//   .Container {
-//     width: 1200px;
-//   }
-// }
